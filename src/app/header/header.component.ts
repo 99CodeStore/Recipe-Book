bfd4dc2712295f88e852363b6b55c5e624f50c78
Bheemsen Dingh <bheemstn@gmail.com>
2021-10-06 17:28:14 +0530

    Recipe Detail and Shoping-Edit Components implementation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Output() featureSelected = new EventEmitter<string>();
+  
   constructor() { 
     
   }
